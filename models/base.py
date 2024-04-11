81c0ec9e2531dab28a9a303da6c877fbeadad000
tf63 <tfukuda9031@gmail.com>
2024-04-11 18:11:40 +0900

    fix device var

--- models/base.py
+++ models/base.py
@@ -26,7 +26,7 @@ class BaseLearner(object):
         self._memory_size = args["memory_size"]
         self._memory_per_class = args.get("memory_per_class", None)
         self._fixed_memory = args.get("fixed_memory", False)
-        self._device = "cuda:0"
+        self._device = args["device"][0]
         self._multiple_gpus = args["device"]
 
     @property
